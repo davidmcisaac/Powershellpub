8983a33427ddac1904b6a36a61bb8ca466c48fd3
davidmcisaac <github@mcisaacit.com>
2017-05-05 16:48:55 +0100

    Create Resolve_ComputerName_2_IP.ps1
    Using Test-connection cmdlet in powershell rather than ping

---
+++ Resolve_ComputerName_2_IP.ps1
@@ -0,0 +1,25 @@
+<#Created by DM 050517
+Resolve_ComputerName_2_IP.ps1 
+#>
+$comp =@();$rep =@();$cp =@()
+$IPNull = "N/A"
+$PingSuccess ="Online"
+$PingnonSuccess ="Offline"
+$comp = Get-ADComputer -Filter * |Where-Object {$_.enabled -eq $true}|select DNSHostName
+foreach ($cp in $comp)
+    {#Loop through each computer found
+        $t=@()
+        $catcherror=@()
+        $t= Test-Connection -ComputerName $cp.DNSHostName -Count 1 -ErrorAction SilentlyContinue -ErrorVariable CatchError
+        if ([string]::IsNullOrEmpty($catcherror))
+            {#Test Connection received reply. Write IP to csv
+            write-host "null"
+            $rep += $t|select @{Name="Computer";exp={[string]$_.Address}},IPv4Address,ipv6address,@{Name="Status";exp={[string]$PingSuccess}}
+            }
+        else
+            {#Write error to csv
+            write-host "not null"
+            $rep += $cp|select @{Name="Computer";exp={$cp.DNSHostName}},@{Name="IPv4Address";exp={[string]$IPNull}},@{Name="IPv6Address";exp={[string]$IPNull}},@{Name="Status";exp={[string]$PingnonSuccess}}
+            }   
+    }
+$rep|export-csv -NoTypeInformation C:\computer_2_ip_report.csv
