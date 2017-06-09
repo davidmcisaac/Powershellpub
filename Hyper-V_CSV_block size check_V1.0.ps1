b3026dc6c4c91f6068332f7d6997477ab1485026
mcisaacit <github@mcisaacit.com>
2017-06-09 13:32:11 +0100

    Hyper-v Block size checks

---
+++ Hyper-V_CSV_block size check_V1.0.ps1
@@ -0,0 +1,5 @@
+$wql = "SELECT Label, Blocksize, Name FROM Win32_Volume WHERE FileSystem='NTFS'"
+Get-WmiObject -Query $wql -ComputerName '.' | Select-Object Label, Blocksize, Name
+
+
+#{get-volume |select filesystemlabel,filesystemtype,allocationunitsize}
