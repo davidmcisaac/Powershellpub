e505631d8a98db6c968684562c5b2f29b15c273b
mcisaacit <github@mcisaacit.com>
2017-06-16 16:39:48 +0100

    export MDT application info into csv file

---
+++ MDT_Export_Application_List_V1.0.ps1
@@ -0,0 +1,23 @@
+﻿<#PSScriptInfo
+.DESCRIPTION 
+Export list of Applications from Microsoft MDT
+.VERSION 1.0
+
+.AUTHOR David McIsaac
+
+.COMPANYNAME 
+
+.COPYRIGHT 2017
+
+.EXTERNALSCRIPTDEPENDENCIES 
+Requires MDT powershell module
+.RELEASENOTES
+ Tested on MDT 8443
+#>
+Import-Module "c:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1" #change if MDT not install on C drive
+New-PSDrive -Name "MDT" -PSProvider MDTProvider -Root "i:\MDTBuild"
+
+$apps =Get-ChildItem "MDT:\Applications"-Recurse
+$appreport=@()
+$appreport= $apps|where-object {$_.NodeType -eq "Application"}|Select-Object Name,shortname,Version,Publisher,enable,language,commandline
+$appreport|export-csv c:\MDT_Applications_list_V1.0.csv
