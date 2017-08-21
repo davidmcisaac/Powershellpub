7585f7ce7b8dc141ef1e71bf5d70c29cde2ace4f
mcisaacit <github@mcisaacit.com>
2017-08-21 17:31:40 +0100

    new script

---
+++ SCCM/SCCM_Export_Queries_V1.0.ps1
@@ -0,0 +1,24 @@
+﻿<#PSScriptInfo
+.DESCRIPTION 
+Export Queries in SCCM 
+.VERSION 1.0
+
+.AUTHOR David McIsaac
+
+.COMPANYNAME 
+
+.COPYRIGHT 2017
+
+.EXTERNALSCRIPTDEPENDENCIES 
+Requires SCCM powershell module
+.RELEASENOTES
+ 
+#>
+Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" # Import the ConfigurationManager.psd1 module 
+
+$pssccm = @();$sccm_site = @();$scquery=@()
+$sccm_site = Get-PSDrive|Where-Object {$_.Provider -match 'CMSite'}|Select-Object Name,sitecode
+$pssccm = ($sccm_site.Name + ":")
+Set-Location $pssccm 
+$scquery =get-cmquery|Select-Object Comments,Expression,LimitToCollectionID,Name,TargetClassName
+$scquery|Export-Csv C:\SCCM_export_queries_V1.0.csv -NoTypeInformation
---
+++ SCCM/SCCM_Import_Queries_V1.0.ps1
@@ -0,0 +1,28 @@
+﻿<#PSScriptInfo
+ .DESCRIPTION 
+Import Queries in SCCM 
+.VERSION 1.0
+
+.AUTHOR David McIsaac
+
+.COMPANYNAME 
+
+.COPYRIGHT 2017
+
+.EXTERNALSCRIPTDEPENDENCIES 
+Requires SCCM powershell module
+.RELEASENOTES
+ 
+#>
+Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" # Import the ConfigurationManager.psd1 module 
+
+$pssccm = @();$sccm_site = @();$scqueryimport=@()
+$sccm_site = Get-PSDrive|Where-Object {$_.Provider -match 'CMSite'}|Select-Object Name,sitecode
+$pssccm = ($sccm_site.Name + ":")
+Set-Location $pssccm 
+$scqueryimport = import-csv "c:\SCCM_queries_V1.0.csv" #import exported queries to array
+foreach ($qry in $scqueryimport)
+  {if ((Get-CMQuery -Name $qry.name) -eq $null) #check if query already exists, ifnot then proceed
+    {New-CMQuery -Name $qry.name -Comment $qry.Comments -Expression $qry.Expression -LimitToCollectionId $qry.LimitToCollectionID -TargetClassName $qry.TargetClassName -Verbose
+    }
+   }
